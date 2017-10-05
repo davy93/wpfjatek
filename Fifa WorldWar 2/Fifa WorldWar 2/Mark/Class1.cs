e2f36e9948af515a3f19e104cc2831a02a3f8188
Márk <Mrk@PC-MASTER-RACE>
2017-10-05 20:31:47 +0200

    Added myFolder

---
+++ Fifa WorldWar 2/Fifa WorldWar 2/Mark/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fifa_WorldWar_2.Mark
+{
+    class Class1
+    {
+    }
+}
